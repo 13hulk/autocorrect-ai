4381d69c3f13a621d2a647d5ad699d77ae0169b0
vikasz <vikas@sensehq.com>
2024-03-04 18:28:29 +0530

    Register health check api

--- autocorrect_ai/__init__.py
+++ autocorrect_ai/__init__.py
@@ -4,4 +4,12 @@ from fastapi import FastAPI
 async def create_app(service: str, testing: bool = False) -> FastAPI:
     app = FastAPI()
 
+    await register_routers(app)
+
     return app
+
+
+async def register_routers(app: FastAPI):
+    from autocorrect_ai.api import health_router
+
+    app.include_router(health_router)
